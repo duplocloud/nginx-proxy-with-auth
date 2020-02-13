660f478a71d34c19c209cce5854ee101d7e6cede
jeeva <jeeva@LAPTOP-BA4AU6B2>
2020-02-13 13:15:54 +0530

    Nginx Auth proxy with username and password

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,20 @@ FROM python:3.5
 
 RUN apt-get update
 RUN apt-get install -y --no-install-recommends \
-        libatlas-base-dev gfortran nginx supervisor
+        libatlas-base-dev gfortran nginx supervisor apache2-utils
 
-RUN pip3 install uwsgi
-
-COPY ./requirements.txt /project/requirements.txt
-
-RUN pip3 install -r /project/requirements.txt
+#RUN pip3 install uwsgi
+#COPY ./requirements.txt /project/requirements.txt
+#RUN pip3 install -r /project/requirements.txt
 
 RUN useradd --no-create-home nginx
 
 RUN rm /etc/nginx/sites-enabled/default
-RUN rm -r /root/.cache
+#RUN rm -r /root/.cache
 
 COPY nginx.conf /etc/nginx/
 COPY flask-site-nginx.conf /etc/nginx/conf.d/
-COPY uwsgi.ini /etc/uwsgi/
+#COPY uwsgi.ini /etc/uwsgi/
 COPY supervisord.conf /etc/
 
 COPY startup.sh /project/
--- startup.sh
+++ startup.sh
@@ -1,17 +1,5 @@
 #!/bin/bash -e
 
-mkdir -m 777 -p /project/flask_cookie
-
-oauth_url=""
-if [ $OAUTH_PROVIDER = 'google' ]
-then
-oauth_url="https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20email\&access_type=offline\&include_granted_scopes=true\&state=$OAUTH_STATE\&redirect_uri=$APP_REDIRECT_HOST%2Fduplo_auth%2Flogin\&response_type=code\&client_id=$OAUTH_CLIENT_ID"
-else
-oauth_url="https://login.microsoftonline.com/$MICROSOFT_AD_DIRECTORY_ID/oauth2/authorize?client_id=$OAUTH_CLIENT_ID\&response_type=code\&redirect_uri=$APP_REDIRECT_HOST/duplo_auth/login\&response_mode=query\&resource=https%3a%2f%2fgraph.windows.net%2f\&state=$OAUTH_STATE"
-fi
-
 sed -ie "s|<PROXY_SERVER_URI>|$PROXY_SERVER_URI|g" /etc/nginx/conf.d/flask-site-nginx.conf
-
-sed -ie "s|<OAUTH_AUTHORIZE_URL>|$oauth_url|g" /etc/nginx/conf.d/flask-site-nginx.conf
-
+htpasswd -c -b /etc/nginx/.htpasswd $USERNAME $PASSWORD
 /usr/sbin/nginx
